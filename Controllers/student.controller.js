88f3a2c4df17c1e25a11b0e82bd6550cba9eb9d6
Sand3shog <sandeshmaharzan5@gmail.com>
2025-01-21 10:58:13 +0545

    updated hashing system

--- Controllers/student.controller.js
+++ Controllers/student.controller.js
@@ -1,55 +1,70 @@
 import express from 'express';
 import Student from "../student.model.js";
-import bcrypt from 'brcypt';
+import bcrypt from 'bcrypt';
+import mongoose from 'mongoose';
 const router =  express.Router();
 
+// register req and password hashing
 router.post("/student/register", async (req, res) => {
     const newStudent = req.body;
+
+    //find student with provided email
+    const student = await Student.findOne({ email: newStudent.email });
+
+    //if student throws error
+    if(student) {
+        return res.status(400).send({message: 'Email already exists'});
+    };
+
+    //hash password =? requirement is plain password and salt rounds
 
     const plainPassword = newStudent.password;
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(plainPassword, saltRounds);
 
-    newUser.password = hashedPassword;
+    newStudent.password = hashedPassword;
 
     await Student.create(newStudent);
     return res.status(201).send("Registered user...")
 })
 
-//router.get("/",(req, res) => {
-//     return res.send('Hello World!');
-// });
-
-//router.post("/posts",(req, res) => {
-//     return res.send('Post your article here! hehe');
-// })
-
-//router.put("/student/update",(req, res) => {
-//     return res.status(201).send({message: 'student updated!!'});
-// })
-
-//router.delete("/student/delete",(req, res) => {
-//     return res.status(200).send({message: 'student deleted!!'});
-// })
-
-// //create an api to insert course
-//router.post("/course/add",(req, res) => {
-//     return res.status(201).send({message: 'course added!!'});
-// })
-
-//router.delete("/course/delete",(req, res) => {
-//     return res.status(200).send({message: 'course delete!!'});
-// })
+// login request
+router.post("/student/login", async (req, res) => {
+    const loginCredentials = req.body;
 
 
-//router.put("/course/update",(req, res) => {
-//     return res.status(200).send({message: 'course updated!!'});
-// })
+    //find student with provided email
+    const student = await Student.findOne({ email : loginCredentials.email });
 
-//router.get("/product/list", (req, res) =>{
-//     return res.status(200).send({message: 'your product here'})
+    //if not student throw error
+    if(!student) {
+        return res.status(400).send({message: 'Invalid email or password'});
+    };
 
-// });
+    //password check
+    // need to compare plain password with hashed password
+    // plain password is provide by student
+
+    const plainPassword = loginCredentials.password;
+    const hashedPassword = student.password;
+
+    const isPasswordMatched = await bcrypt.compare(plainPassword, hashedPassword);
+
+    if(!isPasswordMatched) {
+        return res.status(400).send({message: 'Invalid Credentials'});
+    };
+
+    //remove password
+    student.password = undefined;
+
+    return res.status(200).send({message: 'Login successfull...'});
+});
+
+// list request
+router.get('/student/list', async (req, res) => {
+    const students = await Student.find();
+    return res.status(200).send({message: 'success', student_List: students});
+});
 
 //router.post("/student/add", (req, res) =>{
     
@@ -62,7 +77,7 @@ router.post("/student/register", async (req, res) => {
 //     return res.status(201).send({message: "added student successfully.."});
 // })
 
-// //get actor by id
+//get student by id
 //router.get("/student/details/:id", (req, res) =>{
     
 //     const studentId = req.params.id;
@@ -82,46 +97,33 @@ router.post("/student/register", async (req, res) => {
 // })
 
 
-// //route
-// app.post('/student/add', async (req, res) => {
-//     //extract new student from req body
-//     const studentList = req.body
-//     await Student.create(studentList)
-//     return res.status(201).send({message:"success"})
-// });
+//delete student by id
+router.delete('/student/delete/:id', async (req, res) => {
+    //extract student id from req params
+    const StudentId = req.params.id;
 
-// //get
-// app.get('/student/list', async (req, res) => {
-//     const students = await Student.find();
-//     return res.status(200).send({message: 'success', student_List: students});
-// });
+    //should be a valid mongo id
+    const isValidId = mongoose.isValidObjectId(StudentId);
 
-// //delete student by id
-// app.delete('/student/delete/:id', async (req, res) => {
-//     //extract product id from req params
-//     const StudentId = req.params.id;
-
-//     // should be a valid mongo id
-//     const isValidId = mongoose.isValidObjectId(StudentId);
-
-//     // if not Valid id
-//     if(!isValidId){
-//         return res.status(400).send({message: 'Invalid Object ID...'});
-//     }
+    // if not Valid id
+    if(!isValidId){
+        return res.status(400).send({message: 'Invalid Object ID...'});
+    }
     
-//     // find student
-//     const student = await Student.findOne({_id : StudentId});
+    // find student
+    const student = await Student.findOne({_id : StudentId});
 
-//     // if not student throw error
-//     if(!student) {
-//         return res.status(404).send({message: 'student does not exist'});
-//     }
+    // if not student throw error
+    if(!student) {
+        return res.status(404).send({message: 'student does not exist'});
+    }
 
-//     //delete student
-//     await Student.deleteOne({_id : StudentId
-//     });
+    //delete student
+    await Student.deleteOne({_id : StudentId
+    });
 
-//     // send res
-//     return res.status(200).send({message: 'Student is deleted successfully'});
-// });
+    // send res
+    return res.status(200).send({message: 'Student is deleted successfully'});
+});
+
 export {router as studentController};
--- student.model.js
+++ student.model.js
@@ -2,12 +2,56 @@
 import mongoose from 'mongoose';
 
 const studentSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    age: Number,
-    address: String,
-    dateOfBirth: Date,
-    gender: String,
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 255,
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 100,
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 100,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 100,
+        unique: true,
+    },
+    age: {
+        type: String,
+        required: false,
+        trim: true,
+        maxlength: 100,
+    },
+    address: {
+        type: String,
+        required: false,
+        trim: true,
+        maxlength: 100,
+    },
+    dateOfBirth: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 100,
+    },
+    gender: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 100,
+        enum: ['Male', 'Female', 'Others'],   //select only one in given array
+    },
     hasScholarship: Boolean,
 });
 
