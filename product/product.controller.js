53f3c0292c3a24c05181369e30199fe02cd8fdcd
Sand3shog <sandeshmaharzan5@gmail.com>
2025-01-27 11:24:08 +0545

    update

--- product/product.controller.js
+++ product/product.controller.js
@@ -64,4 +64,108 @@ router.delete('/product/delete/:id', isStudent,  validateMongoIdFromReqParams, a
     return res.status(200).send({message: 'Product is deleted successfully'});
 });
 
+// list products
+router.post(
+    '/product/list',
+    isStudent,
+    async (req, res, next) => {
+      const paginationSchema = Yup.object({
+        page: Yup.number().positive().integer().default(1),
+        limit: Yup.number().positive().integer().default(10),
+      });
+  
+      try {
+        req.body = await paginationSchema.validate(req.body);
+      } catch (error) {
+        return res.status(400).send({ message: error.message });
+      }
+      next();
+    },
+    async (req, res) => {
+      // extract page and limit from req.body
+      const paginationData = req.body;
+  
+      const limit = paginationData.limit;
+      const page = paginationData.page;
+  
+      const skip = (page - 1) * limit;
+  
+      const products = await productTable.aggregate([
+        {
+          $match: {},
+        },
+  
+        { $skip: skip },
+        { $limit: limit },
+      ]);
+  
+      const totalItem = await ProductTable.find().countDocuments();
+  
+      const totalPage = Math.ceil(totalItem / limit);
+  
+      return res
+        .status(200)
+        .send({ message: 'success', productList: products, totalPage });
+    }
+  );
+  
+router.put(
+    "/product/edit/:id",
+    isUser,
+    validateMongoIdFromReqParams,
+    async (req, res, next) => {
+      // create schema
+      const productValidationSchema = Yup.object({
+        name: Yup.string().required().trim().max(155),
+        brand: Yup.string().required().trim().max(155),
+        price: Yup.number().required().min(0),
+        quantity: Yup.number().required().min(1),
+        category: Yup.string()
+          .required()
+          .trim()
+          .oneOf([
+            "grocery",
+            "electronics",
+            "electrical",
+            "clothing",
+            "kitchen",
+            "kids",
+          ]),
+        image: Yup.string().notRequired().trim(),
+      });
+      try {
+        req.body = await productValidationSchema.validate(req.body);
+        next();
+      } catch (error) {
+        return res.status(400).send({ message: error.message });
+      }
+    },
+  
+    async (req, res) => {
+      const productId = req.params.id;
+      // find product
+      const product = await productTable.findOne({ _id: productId });
+  
+      if (!product) {
+        return res.status(404).send({ message: "Product does not exist...." });
+      }
+  
+      // update product
+      const newValues = req.body;
+      await ProductTable.updateOne({_id:productId}),
+      {
+        $set:{
+          name: newValues.name,
+          brand:newValues.brand,
+          price:newValues.price,
+          quantity:newValues.quantity,
+          category:newValues.category,
+        }
+      }
+      return res
+        .status(200)
+        .send({ message: "Product updated successfully...." });
+    }
+  );
+
 export { router as productController};
